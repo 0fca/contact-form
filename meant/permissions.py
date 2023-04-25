063b3217091857ec02f3c0082e60823acc7766cb
Lukas <lukasbownik99@gmail.com>
2023-04-25 20:32:21 +0200

    Update permissions.py

--- meant/permissions.py
+++ meant/permissions.py
@@ -4,6 +4,12 @@ from rest_framework.permissions import BasePermission
 class AdminOnly(BasePermission):
 
     def has_permission(self, request, view):
+        # Okay, this works. 
+        # And what in case if we would like to add far more views than one and far more than there are admin only views? Bunch of ifs?
+        # It is always better to enumarate what needs an elevated access and all other just default to False or True.
+        # What's more it is always more clear in such notation to enumerate actions which need elevated access, so it is visible just by looking over here
+        # So, to keep it clean - add a global variable with view.action name strings which need elevated access and iterate through it over here - if view.action 
+        # is in there and the user is super user then you allow it, otherwise - True or False.
         if view.action == 'create':
             return True
         return request.user.is_superuser
